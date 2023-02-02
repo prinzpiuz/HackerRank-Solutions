034d89302c9ff9414840e33b9a5720d6f6f17761
prinzpiuz <prinzpiuz@gmail.com>
2023-02-02 19:23:09 +0530

    Solution for counting valleys problem

---
+++ counting_valleys.py
@@ -0,0 +1,27 @@
+"""Solution for counting valleys problem
+
+NB: This is not solution i found
+"""
+
+
+def countingValleys(steps: int, path: str) -> int:
+    """To find the valleys travesed from the string path.
+
+    Args:
+        steps (int): Total no of steps taken.
+        path (str): Path String.
+
+    Returns:
+        int: Count of valleys traversed.
+    """
+    height = 0
+    valleys = 0
+    for step in path:
+        if step == "D" and height == 0:
+            height -= 1
+            valleys += 1
+        elif step == "D":
+            height -= 1
+        else:
+            height += 1
+    return valleys
